594bd6cbc9a19dc4205ed53976350f5a8830a94c
Karthik Rams <rkarthik.nitt@gmail.com>
2021-02-17 00:03:23 +0530

    Change member spelling, removed members button css,js from html and inserted in assets

---
+++ assets/js/js.members-photos-hide-show.js
@@ -0,0 +1,40 @@
+var morePics1 = document.getElementById("more1");
+var btnText1 = document.getElementById("myBtn1");
+var morePics2 = document.getElementById("more2");
+var btnText2 = document.getElementById("myBtn2");
+
+
+
+function myFunction1() {
+
+	if(btnText2.innerHTML === "Close") {
+		morePics2.style.display = "none";
+		btnText2.innerHTML = "Deputy Managers";
+	}
+
+  if (morePics1.style.display === "none") { 
+	morePics1.style.display = "flex";
+	btnText1.innerHTML = "Close";
+    
+  } else {
+	morePics1.style.display = "none";
+    btnText1.innerHTML = "Managers"; 
+  }
+};
+
+function myFunction2() {
+
+	if(btnText1.innerHTML === "Close") {
+		morePics1.style.display = "none";
+		btnText1.innerHTML = "Managers";
+	}
+	
+	if (morePics2.style.display === "none") {
+	btnText2.innerHTML = "Close"; 
+	morePics2.style.display = "flex";
+
+	} else {
+	btnText2.innerHTML = "Deputy Managers"; 
+	morePics2.style.display = "none";
+	}
+};
