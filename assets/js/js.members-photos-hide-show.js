4019bd8b539afe11d835565be3855949447f5d3c
Rajneesh Pandey <rajneeshpandey1708@gmail.com>
2021-08-29 13:50:28 +0530

    update managers

--- assets/js/js.members-photos-hide-show.js
+++ assets/js/js.members-photos-hide-show.js
@@ -1,16 +1,16 @@
 var morePics1 = document.getElementById("more1");
 var btnText1 = document.getElementById("myBtn1");
-var morePics2 = document.getElementById("more2");
-var btnText2 = document.getElementById("myBtn2");
+//var morePics2 = document.getElementById("more2");
+//var btnText2 = document.getElementById("myBtn2");
 
 
 
 function myFunction1() {
 
-	if(btnText2.innerHTML === "Close") {
-		morePics2.style.display = "none";
-		btnText2.innerHTML = "Deputy Managers";
-	}
+	//if(btnText2.innerHTML === "Close") {
+	//	morePics2.style.display = "none";
+	//	btnText2.innerHTML = "Deputy Managers";
+	//}
 
   if (morePics1.style.display === "none") { 
 	morePics1.style.display = "flex";
@@ -22,19 +22,19 @@ function myFunction1() {
   }
 };
 
-function myFunction2() {
-
-	if(btnText1.innerHTML === "Close") {
-		morePics1.style.display = "none";
-		btnText1.innerHTML = "Managers";
-	}
-	
-	if (morePics2.style.display === "none") {
-	btnText2.innerHTML = "Close"; 
-	morePics2.style.display = "flex";
-
-	} else {
-	btnText2.innerHTML = "Deputy Managers"; 
-	morePics2.style.display = "none";
-	}
-};
+//function myFunction2() {
+//
+//	if(btnText1.innerHTML === "Close") {
+//		morePics1.style.display = "none";
+//		btnText1.innerHTML = "Managers";
+//	}
+//	
+//	if (morePics2.style.display === "none") {
+//	btnText2.innerHTML = "Close"; 
+//	morePics2.style.display = "flex";
+//
+//	} else {
+//	btnText2.innerHTML = "Deputy Managers"; 
+//	morePics2.style.display = "none";
+//	}
+//};
