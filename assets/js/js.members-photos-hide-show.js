ab0e0dd7d9c29b99e5e4f9e2873ef365685ec70c
Ayush Shukla <34869001+Aayush-Shukla@users.noreply.github.com>
2022-01-22 17:47:53 +0530

    add new initiatives (#7)
    * add RR 2.0 and change member roles (#6) (#1)
    
    * RR 2.0 photo
    
    * update member and add mela,helpdesk
    
    * fix fac-ad piics
    
    * update memberes
    
    * add member

--- assets/js/js.members-photos-hide-show.js
+++ assets/js/js.members-photos-hide-show.js
@@ -4,8 +4,11 @@ var morePics2 = document.getElementById("more2");
 var btnText2 = document.getElementById("myBtn2");
 var morePics3 = document.getElementById("more3");
 var btnText3 = document.getElementById("myBtn3");
+var btnText4 = document.getElementById("myBtn4");
+var morePics4 = document.getElementById("more4");
 var loadIcon = document.getElementById("img-loading")
 var tout; // timeout variable for a timeout which calls a function that checks if all the images are loaded. If loaded:show all the images:play circle animation// 
+
 function myFunction1() {
 	if (tout !== undefined) //checking if a timeout exists. This check is present in all the funtions in this js//
 		clearTimeout(tout)
@@ -15,7 +18,11 @@ function myFunction1() {
 	} else if (btnText3.innerHTML === "Close") {
 		morePics3.style.display = "none";
 		btnText3.innerHTML = "Core 20-21";
+	} else if (btnText4.innerHTML === "Close") {
+		morePics4.style.display = "none";
+		btnText4.innerHTML = "Faculty Advisors";
 	}
+
 	if (morePics1.style.display === "none" && btnText1.innerHTML != "Close") {
 		btnText1.innerHTML = "Close";
 		let elemdiv = document.querySelector("#more1");
@@ -47,7 +54,11 @@ function myFunction2() {
 	} else if (btnText3.innerHTML === "Close") {
 		morePics3.style.display = "none";
 		btnText3.innerHTML = "Core 20-21";
+	} else if (btnText4.innerHTML === "Close") {
+		morePics4.style.display = "none";
+		btnText4.innerHTML = "Faculty Advisors";
 	}
+
 	if (morePics2.style.display === "none" && btnText2.innerHTML != "Close") {
 		btnText2.innerHTML = "Close";
 		let elemdiv = document.querySelector("#more2");
@@ -79,7 +90,11 @@ function myFunction3() {
 	} else if (btnText2.innerHTML === "Close") {
 		morePics2.style.display = "none";
 		btnText2.innerHTML = "Deputy Managers";
+	} else if (btnText4.innerHTML === "Close") {
+		morePics4.style.display = "none";
+		btnText4.innerHTML = "Faculty Advisors";
 	}
+
 	if (morePics3.style.display === "none" && btnText3.innerHTML != "Close") {
 		btnText3.innerHTML = "Close";
 		let elemdiv = document.querySelector("#more3");
@@ -98,6 +113,42 @@ function myFunction3() {
 	} else {
 		morePics3.style.display = "none";
 		btnText3.innerHTML = "Core 20-21";
+		loadIcon.style.display = "none";
+		clearAllTimeout(tout);
+	}
+};
+function myFunction4() {
+	if (tout !== undefined)
+		clearTimeout(tout)
+	if (btnText1.innerHTML === "Close") {
+		morePics1.style.display = "none";
+		btnText1.innerHTML = "Managers";
+	} else if (btnText2.innerHTML === "Close") {
+		morePics2.style.display = "none";
+		btnText2.innerHTML = "Deputy Managers";
+	} else if (btnText3.innerHTML === "Close") {
+		morePics3.style.display = "none";
+		btnText3.innerHTML = "Core 20-21";
+	}
+	
+	if (morePics4.style.display === "none" && btnText4.innerHTML != "Close") {
+		btnText4.innerHTML = "Close";
+		let elemdiv = document.querySelector("#more4");
+		let elems = elemdiv.querySelectorAll("img");
+		let elemsCount = elems.length;
+		(function loadCheck() {
+			let loaded = isLazyLoaded(elems, elemsCount)
+			if (!loaded) {
+				loadIcon.style.display = "inline-block";
+				tout = setTimeout(loadCheck, 1000)
+			} else {
+				morePics4.style.display = "flex";
+				loadIcon.style.display = "none";
+			}
+		}());
+	} else {
+		morePics4.style.display = "none";
+		btnText4.innerHTML = "Faculty Advisors";
 		loadIcon.style.display = "none";
 		clearAllTimeout(tout);
 	}
